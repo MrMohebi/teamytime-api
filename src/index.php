b376da51e367cd7b26722e4167a11a922e7a7dc3
MMMoehebi <MMMohebi@outlook.com>
2022-02-02 09:50:54 +0330

    test db

---
+++ src/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$client = new MongoDB\Client(
+    'mongodb+srv://root:dbtimePass@vpn.devmrm.ir/test?retryWrites=true&w=majority'
+);
+
+$db = $client->test;
