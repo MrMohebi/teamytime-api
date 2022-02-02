7615f16907415e04783cf50bc718288a9c0b5667
MMMoehebi <MMMohebi@outlook.com>
2022-02-02 09:59:39 +0330

    fix

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $client = new MongoDB\Client(
     'mongodb+srv://root:dbtimePass@vpn.devmrm.ir/test?retryWrites=true&w=majority'
