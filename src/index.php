09049611fa75d2611d53f4d69f85c634a2938edc
MMMoehebi <MMMohebi@outlook.com>
2022-02-03 14:11:07 +0330

    t

--- src/index.php
+++ src/index.php
@@ -1,8 +1,13 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
 
-$client = new MongoDB\Client(
-    'mongodb+srv://root:dbtimePass@vpn.devmrm.ir/test?retryWrites=true&w=majority'
-);
+$files2 = scandir(__DIR__.'/../', 1);
 
-$db = $client->test;
+print_r($files2);
+
+//require_once __DIR__ . '/../vendor/autoload.php';
+//
+//$client = new MongoDB\Client(
+//    'mongodb+srv://root:dbtimePass@vpn.devmrm.ir/test?retryWrites=true&w=majority'
+//);
+//
+//$db = $client->test;
