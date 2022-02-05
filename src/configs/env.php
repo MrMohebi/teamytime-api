08938b035682f4abe8413597b5a0f31f8d17d147
MMMoehebi <MMMohebi@outlook.com>
2022-02-05 15:11:43 +0330

    configs

--- src/configs/env.php
+++ src/configs/env.php
@@ -1,3 +1,5 @@
 <?php
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/../../");
 $dotenv->load();
