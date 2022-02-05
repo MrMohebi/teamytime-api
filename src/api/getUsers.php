859ae8bedc0243597fde3af59dc9604bb5675e3d
MMMoehebi <MMMohebi@outlook.com>
2022-02-05 15:28:42 +0330

    getUsers.php

---
+++ src/api/getUsers.php
@@ -0,0 +1,14 @@
+<?php
+require_once "../configs/index.php";
+
+if (isset($client)) {
+    $usersCollection = $client->selectCollection($_ENV['DB_NAME'], 'users');
+    $users = $usersCollection->find();
+
+    $result = [];
+    foreach ($users as $eUser){
+        $result[] = ["_id"=>(string)$eUser['_id'], "name"=>$eUser["name"]];
+    }
+    print_r($result);
+}
+
