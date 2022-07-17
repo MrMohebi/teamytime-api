5e1fe9e4bb6046b3564b00024cdc43680f587234
MrM <MMMohebi@outlook.com>
2022-07-17 14:33:56 +0430

    upsertReport

---
+++ src/api/upsertReport.php
@@ -0,0 +1,45 @@
+<?php
+require_once "../configs/index.php";
+
+if (isset($client) && isset($_POST["userID"]) && isset($_POST["companyID"])) {
+    $reportsCollection = $client->selectCollection($_ENV['DB_NAME'], 'reports');
+    $report = $reportsCollection->findOne([
+        "userID"=>$_POST["userID"],
+        "companyID"=>$_POST["companyID"],
+        "jalaliDate"=>$_POST["jalaliDate"],
+    ]);
+
+    if(isset($report)){
+        $reportsCollection->updateOne(
+            [
+                "userID"=>$_POST["userID"],
+                "companyID"=>$_POST["companyID"],
+                "jalaliDate"=>$_POST["jalaliDate"]
+            ]
+            ,
+            [ '$set' =>
+                [
+                    "timeFields"=>$_POST["timeFields"] ? json_decode($_POST["timeFields"]) : [],
+                    "textFields"=>$_POST["textFields"] ? json_decode($_POST["textFields"]) : [],
+                    "updatedAt"=>time()
+                ]
+            ]
+
+        );
+    }else{
+        $reportsCollection->insertOne([
+            "userID"=>$_POST["userID"],
+            "companyID"=>$_POST["companyID"],
+            "timeFields"=>$_POST["timeFields"] ? json_decode($_POST["timeFields"]) : [],
+            "textFields"=>$_POST["textFields"] ? json_decode($_POST["textFields"]) : [],
+            "jalaliDate"=>$_POST["jalaliDate"],
+            "createdAt"=>time()
+        ]);
+    }
+
+
+    exit("200");
+}
+exit("400");
+
+
