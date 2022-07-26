249949c75cf627952850c4c2030bf2e2b0e2837c
MrM <MMMohebi@outlook.com>
2022-07-26 14:48:48 +0430

    fix cors

--- src/configs/index.php
+++ src/configs/index.php
@@ -3,5 +3,10 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 date_default_timezone_set("Asia/Tehran");
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+
 require_once "mongodb.php";
 require_once "env.php";
