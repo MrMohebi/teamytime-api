a022b830b1d4add5e97bceab26a6609c7d40b170
mmmohebi <MMMohebi@outlook.com>
2022-07-30 16:01:37 +0430

    absolute path autoreport

--- src/api/autoReport.php
+++ src/api/autoReport.php
@@ -1,10 +1,15 @@
 <?php
-require_once "../configs/index.php";
 
-if(isset($client)){
-    $autoReportsCollection = $client->selectCollection($_ENV['DB_NAME'], 'autoReports');
+require_once __DIR__ . '/../../vendor/autoload.php';
 
-    $autoReportsCollection->updateOne(['name'=>'test'], ['$inc'=>['count'=>1]], ['upsert'=>true]);
-    echo 'done';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/../../");
+$dotenv->load();
 
-}
+$client = new MongoDB\Client(
+    "mongodb://".$_ENV['DB_USER'].":".$_ENV['DB_PASS']."@".$_ENV['DB_URL']."/?retryWrites=true&w=majority"
+);
+
+$autoReportsCollection = $client->selectCollection($_ENV['DB_NAME'], 'autoReports');
+
+$autoReportsCollection->updateOne(['name'=>'test'], ['$inc'=>['count'=>1]], ['upsert'=>true]);
+$output = shell_exec('ls');
